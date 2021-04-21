8d9e67ce0fe9786d67a2ab31142ac62c1d7d8cdc
Chularev <chulgarev@gmail.com>
2021-04-22 00:11:06 +0700

    done cheched gradient

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import collections
 
 def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     '''
@@ -32,7 +32,16 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
+
+        x2 = x.copy()
+        x3 = x.copy()
+        x2[ix] = x2[ix] + delta
+        x3[ix] = x3[ix] - delta
+        
+        sum, _ = f(x2)
+        dif, _ = f(x3)
+
+        numeric_grad_at_ix = (sum - dif) / (2 * delta)
 
         # TODO compute value of numeric gradient of f to idx
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
@@ -43,7 +52,3 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
 
     print("Gradient check passed!")
     return True
-
-        
-
-        
