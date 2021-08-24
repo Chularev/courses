53c0e7063e7753341786dd97e0c937b7e227ef35
Chularev <chulgarev@gmail.com>
2021-08-24 18:06:20 +0700

    copy from ather lab

--- assignments/assignment3/metrics.py
+++ assignments/assignment3/metrics.py
@@ -1,13 +1,21 @@
-def binary_classification_metrics(prediction, ground_truth):
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
-
-    # TODO: implement metrics!
-    
-    return accuracy, precision, recall, f1
+import numpy as np
 
 
 def multiclass_accuracy(prediction, ground_truth):
-    return 0
+    """
+    Computes metrics for multiclass classification
+
+    Arguments:
+    prediction, np array of int (num_samples) - model predictions
+    ground_truth, np array of int (num_samples) - true labels
+
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    """
+
+    # TODO: Implement computing accuracy
+
+    res = sum(np.equal(prediction, ground_truth))
+    accuracy = res / len(prediction)
+    return accuracy
+
